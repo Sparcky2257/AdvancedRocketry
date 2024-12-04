efebea02de67e6480ba6fa6e628b734d265c0ccb
Sparcky <Lloydplays@iCloud.com>
2024-12-04 19:08:58 -0400

    A.0.0.3

--- src/main/java/com/example/examplemod/LibreRocktry.java
+++ src/main/java/com/example/examplemod/LibreRocktry.java
@@ -29,11 +29,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(LibreRocktry.MODID)
-public class LibreRocktry
+@Mod(LibreRocktry.MOD_ID)
+public class LibreRocktry 
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "librerocktry_a_spacemod";
+    public static final String MOD_ID = "librerocktry_a_spacemod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
@@ -53,10 +53,6 @@ public class LibreRocktry
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-
     }
 
     // Add the example block item to the building blocks tab
@@ -68,20 +64,16 @@ public class LibreRocktry
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+
         }
     }
 }
